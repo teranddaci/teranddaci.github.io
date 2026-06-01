9d527b1ef945903e0a19a4831271860d3fce5ca4
teranddaci <terand.daci@cit.edu.al>
2026-06-01 21:37:29 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-window.addEventListener("load", () => {
-    document.getElementById("loader").style.display = "none";
+window.addEventListener("load", function () {
+    const loader = document.getElementById("loader");
+    const body = document.body;
+
+    if (loader) {
+        loader.style.opacity = "0";
+        loader.style.transition = "opacity 0.5s ease";
+
+        setTimeout(() => {
+            loader.style.display = "none";
+        }, 500);
+    }
+
+    body.style.opacity = "1";
 });
